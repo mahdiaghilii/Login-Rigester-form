b7398e2f7ae54006e194cc0d84ed43f9ddae144d
mahdi aghili <mahdi.agheyli@gmail.com>
2025-11-29 23:55:49 +0330

    creat error for inputs and creat and save data in the localstorage

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+// creat dark and light mode
+
 const Mode = document.getElementById('MOOD')
 const BODY = document.body
 
 Mode.addEventListener('click' , function(e){
-    console.log(e.target.innerText)
+    
     if(e.target.innerText === 'LIGHT MODE'){
         BODY.classList.add('light')
         BODY.classList.remove('dark')
@@ -13,5 +15,44 @@ Mode.addEventListener('click' , function(e){
         e.target.innerText ='LIGHT MODE'
     }
 })
-
 // creat dark and light mode
+
+let Username = document.getElementById('userName') //select input
+let Password = document.getElementById('password') //select input
+let submit = document.getElementById('submit') //select submit
+let userArr = []
+let userNameArr = []
+let passwordArr = []
+
+
+
+Username.addEventListener('blur' , function(e){                         //creat error for Duplicate username
+    if(userNameArr.includes(e.target.value)){
+        alert('this name is already taken,please choose another name')
+    }
+})
+
+Password.addEventListener('blur' , function(e){
+
+})
+
+submit.addEventListener('click' , function(e){
+    userNameArr.push(Username.value)
+    passwordArr.push(Password.value)
+    
+    let userObj = {}                        
+    userObj.name = Username.value
+    userObj.password = Password.value
+    userArr.push(userObj)
+    console.log(userArr)
+    
+    let struserArr = JSON.stringify(userArr) //save in localstorage
+    localStorage.setItem('DYKO' , struserArr) //save in localstorage
+    console.log(userNameArr)
+
+})
+
+
+
+
+
